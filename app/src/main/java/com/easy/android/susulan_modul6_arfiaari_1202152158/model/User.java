f599224d48bccffb9cad3452c648ebd9c9740d18
arfiaari <arfia.ari@gmail.com>
2018-04-22 12:23:18 +0700

    susulan

---
+++ app/src/main/java/com/easy/android/susulan_modul6_arfiaari_1202152158/model/User.java
@@ -0,0 +1,29 @@
+package com.easy.android.susulan_modul6_arfiaari_1202152158.model;
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
---
+++ app/src/test/java/com/easy/android/susulan_modul6_arfiaari_1202152158/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.easy.android.susulan_modul6_arfiaari_1202152158;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
